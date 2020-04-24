8ac3e75a85b4af8581309df02bd20eb893442434
Florian Kunneman <f.a.kunneman@vu.nl>
2020-04-24 11:54:13 +0200

    added image display (card and list) to richResponse; tested on dialogFlow

--- df_smoothbot/views.py
+++ df_smoothbot/views.py
@@ -21,7 +21,7 @@ def webhook(request):
     # build request object
     req = json.loads(request.body)
     query = req.get('queryResult')
-    fulfillmentText, suggestions, output_contexts = dm.manage(query)
+    fulfillmentText, fulfillmentImg, suggestions, output_contexts = dm.manage(query)
     aog = actions_on_google_response()
     aog_sr = aog.simple_response([
        [fulfillmentText, fulfillmentText, False]
@@ -29,7 +29,21 @@ def webhook(request):
     aog_sc = aog.suggestion_chips(suggestions)
     ff_response = fulfillment_response()
     ff_text = ff_response.fulfillment_text(fulfillmentText)
-    ff_messages = ff_response.fulfillment_messages([aog_sr, aog_sc])
+    if fulfillmentImg:
+        print(fulfillmentImg)
+        if len(fulfillmentImg['list']) == 1:
+            img = fulfillmentImg['list'][0]
+            aog_card = aog.basic_card(img["title"],"",img["description"],[img['image']['url'],img['image']['accessibilityText']],"")
+            ff_messages = ff_response.fulfillment_messages([aog_sr, aog_card, aog_sc])
+        else:
+            inputlist = []
+            for i,img in enumerate(fulfillmentImg['list']):
+                inputlist.append([img['title'],img['description'],[img['optionInfo']['key'],img['optionInfo']['synonyms']],[img['image']['url'],img['image']['accessibilityText']]])
+            img_list = aog.list_select(fulfillmentImg['title'],inputlist)
+            img_list['@type'] = "type.googleapis.com/google.actions.v2.OptionValueSpec"
+            ff_messages = ff_response.fulfillment_messages([aog_sr, img_list, aog_sc])
+    else:
+        ff_messages = ff_response.fulfillment_messages([aog_sr, aog_sc])
     ff_context = ff_response.output_contexts(req.get('session'),output_contexts)
     reply = ff_response.main_response(ff_text, ff_messages, ff_context)
     # return generated response
